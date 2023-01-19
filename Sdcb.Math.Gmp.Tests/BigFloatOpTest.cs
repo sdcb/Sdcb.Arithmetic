e5a4a184f16d8e2987e69c4418025720c7a5ee4b
sdcb <sdflysha@qq.com>
2023-01-19 14:25:41 +0800

    Update tests.

--- Sdcb.Math.Gmp.Tests/BigFloatOpTest.cs
+++ Sdcb.Math.Gmp.Tests/BigFloatOpTest.cs
@@ -1,99 +1,98 @@
 using Xunit.Abstractions;
 
-namespace Sdcb.Math.Gmp.Tests
+namespace Sdcb.Math.Gmp.Tests;
+
+public class BigFloatOpTest
 {
-    public class BigFloatOpTest
+    private readonly ITestOutputHelper _console;
+
+    public BigFloatOpTest(ITestOutputHelper console)
     {
-        private readonly ITestOutputHelper _console;
+        _console = console;
+    }
 
-        public BigFloatOpTest(ITestOutputHelper console)
-        {
-            _console = console;
-        }
+    [Fact]
+    public void Add()
+    {
+        BigFloat r = BigFloat.Parse("1.5") + BigFloat.Parse("3.25");
+        Assert.Equal(4.75, r.ToDouble());
+    }
 
-        [Fact]
-        public void Add()
-        {
-            BigFloat r = BigFloat.Parse("1.5") + BigFloat.Parse("3.25");
-            Assert.Equal(4.75, r.ToDouble());
-        }
+    [Fact]
+    public void AddInt32()
+    {
+        BigFloat r = BigFloat.Parse("1.5") + 100;
+        Assert.Equal(101.5, r.ToDouble());
+    }
 
-        [Fact]
-        public void AddInt32()
-        {
-            BigFloat r = BigFloat.Parse("1.5") + 100;
-            Assert.Equal(101.5, r.ToDouble());
-        }
+    [Fact]
+    public void Subtract()
+    {
+        BigFloat r = BigFloat.From(7.25) - BigFloat.From(3.125);
+        Assert.Equal(4.125, r.ToDouble());
+    }
 
-        [Fact]
-        public void Subtract()
-        {
-            BigFloat r = BigFloat.From(7.25) - BigFloat.From(3.125);
-            Assert.Equal(4.125, r.ToDouble());
-        }
+    [Fact]
+    public void SubtractInt32()
+    {
+        BigFloat r = BigFloat.From(7.25) - 10;
+        Assert.Equal(-2.75, r.ToDouble());
+    }
 
-        [Fact]
-        public void SubtractInt32()
-        {
-            BigFloat r = BigFloat.From(7.25) - 10;
-            Assert.Equal(-2.75, r.ToDouble());
-        }
+    [Fact]
+    public void SubtractInt32Reverse()
+    {
+        BigFloat r = 10 - BigFloat.From(7.25);
+        Assert.Equal(2.75, r.ToDouble());
+    }
 
-        [Fact]
-        public void SubtractInt32Reverse()
-        {
-            BigFloat r = 10 - BigFloat.From(7.25);
-            Assert.Equal(2.75, r.ToDouble());
-        }
+    [Fact]
+    public void Multiple()
+    {
+        BigFloat r = BigFloat.From(2.5) * BigFloat.From(2.5);
+        Assert.Equal(6.25, r.ToDouble());
+    }
 
-        [Fact]
-        public void Multiple()
-        {
-            BigFloat r = BigFloat.From(2.5) * BigFloat.From(2.5);
-            Assert.Equal(6.25, r.ToDouble());
-        }
+    [Fact]
+    public void MultipleInt32()
+    {
+        BigFloat r = BigFloat.From(2.5) * 2147483647;
+        Assert.Equal(5368709117.5, r.ToDouble());
+    }
 
-        [Fact]
-        public void MultipleInt32()
-        {
-            BigFloat r = BigFloat.From(2.5) * 2147483647;
-            Assert.Equal(5368709117.5, r.ToDouble());
-        }
+    [Fact]
+    public void Divide()
+    {
+        BigFloat r = BigFloat.Parse(long.MinValue.ToString()) / BigFloat.From(int.MinValue);
+        Assert.Equal(1L << 32, r.ToDouble());
+    }
 
-        [Fact]
-        public void Divide()
-        {
-            BigFloat r = BigFloat.Parse(long.MinValue.ToString()) / BigFloat.From(int.MinValue);
-            Assert.Equal(1L << 32, r.ToDouble());
-        }
+    [Fact]
+    public void DivideUInt32()
+    {
+        BigFloat r = BigFloat.Parse((1L << 57).ToString()) / (1u << 31);
+        _console.WriteLine(r.ToString());
+        Assert.Equal(1 << 26, r.ToDouble());
+    }
 
-        [Fact]
-        public void DivideUInt32()
-        {
-            BigFloat r = BigFloat.Parse((1L << 57).ToString()) / (1u << 31);
-            _console.WriteLine(r.ToString());
-            Assert.Equal(1 << 26, r.ToDouble());
-        }
+    [Fact]
+    public void DivideUInt32Reverse()
+    {
+        BigFloat r = 5 / BigFloat.From(1 << 10);
+        Assert.Equal(0.0048828125, r.ToDouble());
+    }
 
-        [Fact]
-        public void DivideUInt32Reverse()
-        {
-            BigFloat r = 5 / BigFloat.From(1 << 10);
-            Assert.Equal(0.0048828125, r.ToDouble());
-        }
+    [Fact]
+    public void PowerInt32()
+    {
+        BigFloat r = BigFloat.From(2.5) ^ 10;
+        Assert.Equal(9536.7431640625, r.ToDouble());
+    }
 
-        [Fact]
-        public void PowerInt32()
-        {
-            BigFloat r = BigFloat.From(2.5) ^ 10;
-            Assert.Equal(9536.7431640625, r.ToDouble());
-        }
-
-        [Fact]
-        public void Negate()
-        {
-            BigFloat r = -BigFloat.From(2.5);
-            Assert.Equal(-2.5, r.ToDouble());
-        }
+    [Fact]
+    public void Negate()
+    {
+        BigFloat r = -BigFloat.From(2.5);
+        Assert.Equal(-2.5, r.ToDouble());
     }
 }
--- Sdcb.Math.Gmp.Tests/RawTest.cs
+++ Sdcb.Math.Gmp.Tests/RawTest.cs
@@ -1,42 +1,41 @@
 using System.Runtime.InteropServices;
 using Xunit.Abstractions;
 
-namespace Sdcb.Math.Gmp.Tests
+namespace Sdcb.Math.Gmp.Tests;
+
+public class RawTest
 {
-    public class RawTest
+    private readonly ITestOutputHelper _console;
+
+    public RawTest(ITestOutputHelper console)
     {
-        private readonly ITestOutputHelper _console;
+        _console = console;
+    }
 
-        public RawTest(ITestOutputHelper console)
-        {
-            _console = console;
-        }
+    [Fact]
+    public unsafe void PiMulE()
+    {
+        IntPtr f1 = Marshal.AllocHGlobal(24);
+        IntPtr f2 = Marshal.AllocHGlobal(24);
+        IntPtr f3 = Marshal.AllocHGlobal(24);
+        GmpNative.__gmpf_init_set_d(f1, 3.14);
+        GmpNative.__gmpf_init_set_d(f2, 2.718);
+        GmpNative.__gmpf_init(f3);
+        GmpNative.__gmpf_mul(f3, f1, f2);
+        double d = GmpNative.__gmpf_get_d(f3);
+        _console.WriteLine(d.ToString());
 
-        [Fact]
-        public unsafe void PiMulE()
-        {
-            IntPtr f1 = Marshal.AllocHGlobal(24);
-            IntPtr f2 = Marshal.AllocHGlobal(24);
-            IntPtr f3 = Marshal.AllocHGlobal(24);
-            GmpNative.__gmpf_init_set_d(f1, 3.14);
-            GmpNative.__gmpf_init_set_d(f2, 2.718);
-            GmpNative.__gmpf_init(f3);
-            GmpNative.__gmpf_mul(f3, f1, f2);
-            double d = GmpNative.__gmpf_get_d(f3);
-            _console.WriteLine(d.ToString());
+        int exp = 0;
+        IntPtr str = GmpNative.__gmpf_get_str(IntPtr.Zero, (IntPtr)(&exp), 10, 0, f3);
+        _console.WriteLine(Marshal.PtrToStringAnsi(str));
+        _console.WriteLine($"exp: {exp}");
 
-            int exp = 0;
-            IntPtr str = GmpNative.__gmpf_get_str(IntPtr.Zero, (IntPtr)(&exp), 10, 0, f3);
-            _console.WriteLine(Marshal.PtrToStringAnsi(str));
-            _console.WriteLine($"exp: {exp}");
-
-            GmpNative.__gmpf_clear(f1);
-            GmpNative.__gmpf_clear(f2);
-            GmpNative.__gmpf_clear(f3);
-            Marshal.FreeHGlobal(f1);
-            Marshal.FreeHGlobal(f2);
-            Marshal.FreeHGlobal(f3);
-            GmpMemory.Free(str);
-        }
+        GmpNative.__gmpf_clear(f1);
+        GmpNative.__gmpf_clear(f2);
+        GmpNative.__gmpf_clear(f3);
+        Marshal.FreeHGlobal(f1);
+        Marshal.FreeHGlobal(f2);
+        Marshal.FreeHGlobal(f3);
+        GmpMemory.Free(str);
     }
 }
