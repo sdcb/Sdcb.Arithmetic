cbdc97f2fa20394d7dbd08979b105455ad1bc188
sdcb <sdflysha@qq.com>
2023-02-08 23:00:38 +0800

    Add first GmpRandomExtensions

---
+++ Sdcb.Arithmetic.Mpfr.Tests/RandomTests.cs
@@ -0,0 +1,25 @@
+﻿using Sdcb.Arithmetic.Gmp;
+using Xunit.Abstractions;
+
+namespace Sdcb.Arithmetic.Mpfr.Tests;
+
+public class RandomTests
+{
+    private readonly ITestOutputHelper _console;
+
+    public RandomTests(ITestOutputHelper console)
+    {
+        _console = console;
+    }
+
+    [Fact]
+    public void FastRandom5()
+    {
+        using GmpRandom r = new();
+        for (int i = 0; i < 5; ++i)
+        {
+            using MpfrFloat f = r.NextMpfrFloat(precision: 100);
+            _console.WriteLine(f.ToString());
+        }
+    }
+}
--- Sdcb.Arithmetic.Mpfr/GmpRandomExtensions.cs
+++ Sdcb.Arithmetic.Mpfr/GmpRandomExtensions.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Sdcb.Arithmetic.Gmp;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Sdcb.Arithmetic.Mpfr
 {
-    public static class GmpRandomExtensions
+    public unsafe static class GmpRandomExtensions
     {
+        public static int Next(this GmpRandom r, MpfrFloat rop)
+        {
+            fixed (GmpRandomState* prandom = &r.Raw)
+            fixed (Mpfr_t* pop = &rop.Raw)
+            {
+                return MpfrLib.mpfr_urandomb((IntPtr)pop, (IntPtr)prandom);
+            }
+        }
 
+        public static MpfrFloat NextMpfrFloat(this GmpRandom r, int? precision = null)
+        {
+            MpfrFloat rop = new(precision ?? MpfrFloat.DefaultPrecision);
+            Next(r, rop);
+            return rop;
+        }
     }
 }
