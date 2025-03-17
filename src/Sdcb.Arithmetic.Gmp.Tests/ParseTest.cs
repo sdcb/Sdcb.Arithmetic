13953b05950da4359825ed195e3571717c22e29d
sdcb <sdflysha@qq.com>
2025-03-17 12:54:29 +0800

    fast test Parse100RandomNumbers

---
+++ src/Sdcb.Arithmetic.Gmp.Tests/ParseTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Sdcb.Arithmetic.Gmp.Tests;
+
+public class ParseTest
+{
+    [Fact]
+    public void Parse100RandomNumbers()
+    {
+        var random = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            int length = random.Next(1, 100);
+            var sb = new StringBuilder();
+            sb.Append(random.Next(1, 10));
+            for (int j = 1; j < length; j++)
+            {
+                sb.Append(random.Next(0, 10));
+            }
+
+            var str = sb.ToString();
+            using GmpFloat gf = GmpFloat.Parse(str);
+            Assert.Equal(str, gf.ToString());
+
+            using GmpInteger gi = GmpInteger.Parse(str);
+            Assert.Equal(str, gi.ToString());
+        }
+    }
+}
